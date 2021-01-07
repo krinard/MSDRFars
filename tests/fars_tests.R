81361838ee3fdf9c0d079fc7fa5a84d1aeb36d98
Rinard <kate.rinard@yahoo.com>
2021-01-07 16:40:54 -0500

    Updating tests to fix errors

--- tests/fars_tests.R
+++ tests/fars_tests.R
@@ -1,7 +1,9 @@
-testthat::test_that("make filename", {
-  filename1 <- MSDRFars::make_filename("2020")
-  testthat::expect_error(basename(filename1), "accident_2020.csv.bz2")
-
-  filename2 <- MSDRFars::make_filename(2012)
-  testthat::expect_equal(basename(filename2), "accident_2012.csv.bz2")
+testthat::test_that("existing file", {
+  filename2 <- MSDRFars::make_filename("2013")
+  testthat::expect_equal(basename(filename2), "accident_2013.csv.bz2")
 })
+
+testthat::test_that("missing file", {
+  testthat::expect_error(MSDRFars::make_filename("2020"))
+})
+
